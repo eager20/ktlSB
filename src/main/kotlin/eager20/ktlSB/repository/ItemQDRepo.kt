0fd7bc7dc7f33cd7538857503b814bb1ca2a6a43
Brandon <eager30@tmon.co.kr>
2022-11-03 11:18:37 +0900

    QueryDSL Update 셈플. em flush, clear 붙는게 신경쓰임. 훔..

--- src/main/kotlin/eager20/ktlSB/repository/ItemQDRepo.kt
+++ src/main/kotlin/eager20/ktlSB/repository/ItemQDRepo.kt
@@ -4,11 +4,22 @@ import com.querydsl.jpa.impl.JPAQueryFactory
 import eager20.ktlSB.Items
 import eager20.ktlSB.QItems.items
 import org.springframework.stereotype.Repository
+import javax.persistence.EntityManager
 
 @Repository
-class ItemQDRepo (private val queryFactory: JPAQueryFactory) {
+class ItemQDRepo(
+    private val queryFactory: JPAQueryFactory,
+    private val em: EntityManager
+) {
 
     fun findOne(id:Long):Items{
         return queryFactory.selectFrom(items).where(items.dealno.eq(id)).fetchOne()!!
+    }
+
+    fun update(id:Long):Long{
+        val updateedId = queryFactory.update(items).set(items.product, "eager20").execute()
+        em.flush()
+        em.clear()
+        return updateedId
     }
 }
--- src/test/kotlin/eager20/ktlSB/repository/JpaTest.kt
+++ src/test/kotlin/eager20/ktlSB/repository/JpaTest.kt
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.TestConstructor
+import org.springframework.transaction.annotation.Transactional
 
 @SpringBootTest
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
@@ -38,10 +39,22 @@ class JpaTest (val itemRespository: ItemRespository, val itemsQDRepo: ItemQDRepo
 
         val Items = Items(1L, "TEST01", 3000, 50)
         itemRespository.save(Items)
-
-
         val itemOne = itemsQDRepo.findOne(1L)
         Assertions.assertThat(itemOne.dealno).isEqualTo(1)
         println(itemOne.toString())
+    }
+
+
+    @Test
+    @Transactional
+    fun jpaQueryDSLUpdateTest(){
+
+        val Items = Items(1L, "TEST01", 3000, 50)
+        itemRespository.save(Items)
+        val updatedDealNo = itemsQDRepo.update(1L)
+
+        val itemOne = itemsQDRepo.findOne(updatedDealNo)
+        Assertions.assertThat(itemOne.product).isEqualTo("eager20")
+        println("## RESULT=========> $itemOne")
     }
 }
